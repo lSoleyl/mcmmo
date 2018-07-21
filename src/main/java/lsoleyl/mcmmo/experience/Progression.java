9de760fb964497e03638cb724c6a3aadf3e043cc
lSoleyl <lSoleyl+github@gmail.com>
2018-07-21 19:59:33 +0200

    Implemented progression curve and a few calculation routines for xp<->level conversion.

---
+++ src/main/java/lsoleyl/mcmmo/experience/Progression.java
@@ -0,0 +1,70 @@
+package lsoleyl.mcmmo.experience;
+
+import static java.lang.Math.sqrt;
+
+/** This class defines the progression curve and some methods to convert between xp and level.
+ *  This class uses a singleton instance which precalculates a few values for faster calculations.
+ *
+ *  The base progression is required xp for a single level = 1000 + 20*level
+ *  This yields in total xp needed for a given level = 1010*level + 10*level^2
+ *
+ *  This in turn gives the inverse of level = (sqrt(xp + 159.69^2) - 159.69) / 3.1622
+ *
+ *  We define 1000 as base and 20 slope, then we can redefine in more general terms:
+ *  xp(level) = base + slope*level
+ *  totalXp(level) = (base+slope/2)*level + (slope/2)*level^2
+ *
+ *  level(totalXp) = (sqrt(xp + z^2) - z) / sqrt(slope/2)
+ *  with z = (base+(slope/2))/sqrt(2*slope)
+*/
+public class Progression {
+    private static final Progression instance = new Progression();
+
+    public final long base = 1000;
+    public final long slope = 20; // must be dividable by 2 for the calculations to work correctly
+    private final double z; // must be a double to correctly calculate th value.
+    private final double z2;
+    private final long halfSlope; // this is used frequently, so precalculate it
+    private final double halfSlopeRoot;
+
+    /** Precalculate a few values
+     */
+    private Progression() {
+        z = (base+slope)/sqrt(2.0 * slope);
+        z2 = z*z;
+        halfSlope = slope / 2;
+        halfSlopeRoot = sqrt(halfSlope);
+    }
+
+    public static Progression getInstance() {
+        return instance;
+    }
+
+    /** Returns the level, which corresponds to the given total xp value
+     *
+     * @param totalXp
+     * @return
+     */
+    public int getLevel(long totalXp) {
+        // rounding down is exactly what we want here
+        return (int)((sqrt(totalXp + z2) - z) / halfSlopeRoot);
+    }
+
+    /** Returns the amount of xp needed to level up from level-1 to level
+     *
+     * @param level the level to reach from level-1
+     * @return the required amount of xp
+     */
+    public long getLevelXp(int level) {
+        return base + slope*level;
+    }
+
+    /** Returns the total amount of xp needed to reach a certain level from level 0
+     *
+     * @param level the level to reach from 0
+     * @return the total amount of xp needed to each that level
+     */
+    public long getTotalXp(int level) {
+        return (base + halfSlope)*level + halfSlope*level*level;
+    }
+}
---
+++ src/main/java/lsoleyl/mcmmo/experience/XPWrapper.java
@@ -0,0 +1,81 @@
+package lsoleyl.mcmmo.experience;
+
+import lsoleyl.mcmmo.data.PlayerXp;
+import lsoleyl.mcmmo.skills.Skill;
+
+import java.util.Optional;
+
+/** This class wraps the PlayerXP object for a specific skill an exposes better interface for reading and modifying
+ *  the value. For better performance each object keeps the current xp value and level cached in a local field so
+ *  it is not advised to create multiple such objects for the same skill.
+ */
+public class XPWrapper {
+    private Skill skill;
+    private PlayerXp xp;
+    private long xpValue; // cache the value in a local field for faster access
+    private int level;    // the current level this gets updated upon construction of this wrapper and when adding xp
+
+
+    public XPWrapper(PlayerXp xp, Skill skill) {
+        this.xp = xp;
+        this.skill = skill;
+        xpValue = xp.get(skill);
+        level = Progression.getInstance().getLevel(xpValue);
+    }
+
+    /** Adds the given xpAmount to this skill. This change is immediately applied to the PlayerXp object.
+     *
+     * @param xpAmount the amount of xp to add.
+     * @return if the xp has caused the level to change, the new level is returned.
+     */
+    public Optional<Integer> addXp(long xpAmount) {
+        xpValue += xpAmount;
+        xp.set(skill, xpValue);
+
+        if (getRemainingLevelXp() <= 0) {
+            level = Progression.getInstance().getLevel(xpValue);
+            return Optional.of(level);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    /** Returns the current total xp value
+     */
+    public long getTotalXp() {
+        return xpValue;
+    }
+
+    /** Returns the currently collected xp towards the next level.
+     *  This value is always smaller than  getLevelXp(level+1)
+     *
+     * @return
+     */
+    public long getCurrentLevelXp() {
+        return xpValue - Progression.getInstance().getTotalXp(level);
+    }
+
+    /** Returns the total amount of xp, the player needs to collect to reach the next level from the current level
+     */
+    public long getNextLevelXp() {
+        return Progression.getInstance().getLevelXp(level+1);
+    }
+
+    /** Returns the remaining xp to reach the next level
+     */
+    public long getRemainingLevelXp() {
+        return Progression.getInstance().getTotalXp(level+1) - xpValue;
+    }
+
+    /** Returns the current level of this skill
+     */
+    public int getLevel() {
+        return level;
+    }
+
+
+    @Override
+    public String toString() {
+        return "level " + level + " (" + getCurrentLevelXp() + " / " + getNextLevelXp() + ") xp";
+    }
+}
