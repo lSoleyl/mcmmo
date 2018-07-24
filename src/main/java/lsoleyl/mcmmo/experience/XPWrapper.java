5c0e0c2c33ba1ca77135a1eab97141dbb51f5bf6
lSoleyl <lSoleyl+github@gmail.com>
2018-07-24 12:54:38 +0200

    Fixed problem with combat skill, which reduced the player's xp if more damage was absorbed than dealt.

--- src/main/java/lsoleyl/mcmmo/experience/XPWrapper.java
+++ src/main/java/lsoleyl/mcmmo/experience/XPWrapper.java
@@ -1,5 +1,6 @@
 package lsoleyl.mcmmo.experience;
 
+import lsoleyl.mcmmo.MCMMO;
 import lsoleyl.mcmmo.data.PlayerXp;
 import lsoleyl.mcmmo.skills.Skill;
 import lsoleyl.mcmmo.utility.ChatFormat;
@@ -16,6 +17,10 @@ public class XPWrapper {
     private long xpValue; // cache the value in a local field for faster access
     private int level;    // the current level this gets updated upon construction of this wrapper and when adding xp
 
+    // since we have only one XPWrapper per player per skill, we can store the skill's cooldown in this object.
+    private long cooldownEnd = 0;
+    public static final int TICKS_PER_SECOND = 20;
+
 
     public XPWrapper(PlayerXp xp, Skill skill) {
         this.xp = xp;
@@ -26,25 +31,28 @@ public class XPWrapper {
 
     /** Adds the given xpAmount to this skill. This change is immediately applied to the PlayerXp object.
      *
-     * @param xpAmount the amount of xp to add.
+     * @param xpAmount the amount of xp to add. If this value is negative, no change is applied to the player's xp
      * @return if the xp has caused the level to change, the new level is returned.
      */
     public Optional<Integer> addXp(long xpAmount) {
+        if (xpAmount <= 0) {
+            return Optional.empty();
+        }
+
         xpValue += xpAmount;
         xp.set(skill, xpValue);
 
-        if (getRemainingLevelXp() <= 0) {
-            level = Progression.getInstance().getLevel(xpValue);
+        int prevLevel = level;
+
+        while (getRemainingLevelXp() <= 0) {
+            ++level;
+        }
+
+        if (level > prevLevel) {
             return Optional.of(level);
         } else {
             return Optional.empty();
         }
-    }
-
-    /** Returns the current total xp value
-     */
-    public long getTotalXp() {
-        return xpValue;
     }
 
     /** Returns the currently collected xp towards the next level.
@@ -72,6 +80,20 @@ public class XPWrapper {
      */
     public int getLevel() {
         return level;
+    }
+
+    /** Returns true if the skill is still on cooldown
+     */
+    public boolean isOnCooldown() {
+        return MCMMO.tickCount < cooldownEnd;
+    }
+
+    /** Sets the skill on cooldown for the given amount of seconds.
+     *
+     * @param seconds
+     */
+    public void setCooldown(int seconds) {
+        cooldownEnd = MCMMO.tickCount + TICKS_PER_SECOND * seconds;
     }
 
 
